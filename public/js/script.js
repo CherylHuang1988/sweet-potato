5b5368c2efdeaa4ca6af07faeb3b3ff535976603
CherylHuang1988 <cherylhuang1988@gmail.com>
2021-09-25 11:22:37 +0200

    Init sweet-potato

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("sweet-potato JS imported successfully!");
+  },
+  false
+);
---
+++ utils/consts.js
@@ -0,0 +1,3 @@
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/sweet-potato";
+
+module.exports = MONGO_URI;
